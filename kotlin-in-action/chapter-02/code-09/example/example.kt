fa2b7a4d8c0ad70457e8b98f697d727f95d8e7c8
Sehui <109209627+SehuiJang@users.noreply.github.com>
2026-06-17 01:56:07 +0900

    Update example.kt

--- kotlin-in-action/chapter-02/code-09/example/example.kt
+++ kotlin-in-action/chapter-02/code-09/example/example.kt
@@ -3,8 +3,10 @@ import ch02.colors.Color.*
 fun measureColor() = ORANGE
 
 fun getWarmthFromSensor() =
+    // The subject of `when` is the return value of `measureColor()`.
+    // Next, the declared variable, `color`, captures the subject.
     when (val color = measureColor()) {
-        RED, ORANGE, YELLOW -> "warm (red = ${color.r})"
+        RED, ORANGE, YELLOW -> "warm (red = ${color.r})" // reusing `color` in each branch
         GREEN -> "neutral (green = ${color.g})"
         BLUE, INDIGO, VIOLET -> "cold (blue = ${color.b})"
     }
